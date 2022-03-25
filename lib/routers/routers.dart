ba679afc974311de04e3fcdf1bd080d46b75a242
nykolayr_tomsk <nykolayr@gmail.com>
2022-03-25 23:27:01 +0700

    begin

---
+++ lib/routers/routers.dart
@@ -0,0 +1,93 @@
+import 'package:Amosov/ui/home/bloc/home_bloc.dart';
+import 'package:Amosov/ui/home/home.dart';
+import 'package:Amosov/ui/settings/bloc/settings_bloc.dart';
+import 'package:Amosov/ui/settings/settings.dart';
+import 'package:Amosov/utils/colors.dart';
+import 'package:Amosov/utils/value.dart';
+import 'package:bart/bart/bart_model.dart';
+import 'package:bart/bart/bart_scaffold.dart';
+import 'package:bart/bart/bottom_bar.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../ui/statistics/statistics.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+final CustomNavigatorObserver<PageRoute> routeObserver =
+    CustomNavigatorObserver<PageRoute>();
+
+Future appPushNamed(String route, {Object? arguments}) =>
+    navigatorKey.currentState!.pushNamed(route, arguments: arguments);
+
+List<BartMenuRoute> subRoutes() {
+  return [
+    BartMenuRoute.bottomBar(
+      label: tr("training"),
+      icon: Icons.home,
+      path: '/training',
+      pageBuilder: (context, settings) => const HomePage(),
+    ),
+    BartMenuRoute.bottomBar(
+      cache: true,
+      label: tr('statistics'),
+      icon: Icons.emoji_events_outlined,
+      path: '/statistics',
+      pageBuilder: (context, settings) => const ProgressPage(),
+    ),
+    BartMenuRoute.bottomBar(
+      label: tr('settings'),
+      icon: Icons.settings,
+      path: '/settings',
+      pageBuilder: (context, settings) => BlocProvider(
+        create: (_) => SettingsBloc(userRepository),
+        child: const SettingsPage(),
+      ),
+    ),
+  ];
+}
+
+Route<dynamic> routes(RouteSettings settings) {
+  switch (settings.name) {
+    case '/':
+      return MaterialPageRoute(
+        builder: (_) => const MainPageMenu(routesBuilder: subRoutes),
+      );
+    default:
+      throw 'unexpected Route';
+  }
+}
+
+class MainPageMenu extends StatefulWidget {
+  final BartRouteBuilder routesBuilder;
+  const MainPageMenu({Key? key, required this.routesBuilder}) : super(key: key);
+
+  @override
+  State<MainPageMenu> createState() => _MainPageMenuState();
+}
+
+class _MainPageMenuState extends State<MainPageMenu> {
+  @override
+  Widget build(BuildContext context) {
+    context.watch<HomeBloc>();
+    return BartScaffold(
+      routesBuilder: widget.routesBuilder,
+      navigatorObservers: [routeObserver],
+      bottomBar: BartBottomBar.fromFactory(
+        elevation: 10,
+        selectedItemColor: AppColor.yellow,
+        unselectedItemColor: Colors.white,
+        bgColor: AppColor.darkBroun,
+        bottomBarFactory: BartMaterialBottomBar.bottomBarFactory,
+      ),
+    );
+  }
+}
+
+class CustomNavigatorObserver<T> extends RouteObserver<PageRoute<T>> {
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+  }
+}
---
+++ lib/ui/profile/profile.dart
@@ -0,0 +1,25 @@
+import 'package:Amosov/ui/widgets/app_bar.dart';
+import 'package:Amosov/utils/colors.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.meddleGrey,
+      appBar: AppBarWithIcon(title: tr('profile')),
+      body: const SizedBox(
+        width: double.infinity,
+        child: Text('profile'),
+      ),
+    );
+  }
+}
